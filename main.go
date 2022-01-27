54e67ad0dd3a06aaa2389660f725ef1d309bb74d
kman@vmware.com <kman@vmware.com>
2022-01-27 23:17:08 +0530

    second commit

--- main.go
+++ main.go
@@ -4,7 +4,81 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
+
+// check ip address valid or not
+func ip4or6(ip string) string {
+
+	if len(ip) == 0 {
+		return "invalid IP"
+	}
+
+	var countdot int = strings.Count(ip, ".")
+
+	var countcolon int = strings.Count(ip, ":")
+
+	if countdot == 3 {
+
+		var sub string = ""
+		ip += "."
+
+		for i := 0; i < len(ip); i++ {
+
+			if ip[i] == '.' {
+
+				num, err := strconv.ParseInt(sub, 10, 64)
+
+				if len(sub) == 0 {
+					return "invalid IP"
+
+				} else if num < 0 || num > 255 {
+					return "invalid IP"
+				} else if err != nil {
+					return "invalid IP"
+				}
+				sub = ""
+
+			} else {
+				sub += string(ip[i])
+
+			}
+		}
+
+		return "valid IPv4"
+
+	} else if countcolon == 7 {
+
+		var sub string = ""
+		ip += ":"
+
+		for i := 0; i < len(ip); i++ {
+
+			if ip[i] == ':' {
+
+				if len(sub) > 4 {
+					return "1 invalid IP"
+				}
+
+				sub = ""
+
+			} else {
+
+				if (ip[i] < 'A' || ip[i] > 'F') && (ip[i] < 'a' && ip[i] > 'f') && (ip[i] < '0' || ip[i] > '9') {
+					return "2 invalid IP"
+				}
+
+				sub += string(ip[i])
+			}
+		}
+		return "valid IPv6"
+
+	} else {
+		return "Invalid IP"
+	}
+
+}
 
 func process(w http.ResponseWriter, r *http.Request) {
 
@@ -17,21 +91,32 @@ func process(w http.ResponseWriter, r *http.Request) {
 	case "GET":
 
 		http.ServeFile(w, r, "form.html")
+
 	case "POST":
 
 		if err := r.ParseForm(); err != nil {
 			fmt.Fprintf(w, "ParseForm() err: %v", err)
 			return
 		}
+		err := r.ParseForm()
+		if err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
 
-		name := r.FormValue("name")
-		occupation := r.FormValue("occupation")
+		}
 
-		fmt.Fprintf(w, "%s is a %s\n", name, occupation)
+		ipaddress := r.FormValue("ip")
+
+		fmt.Fprintf(w, " %s is  %s \n", ipaddress, ip4or6(ipaddress))
+
+		// name := r.FormValue("name")
+		// occupation := r.FormValue("occupation")
+
+		// fmt.Fprintf(w, "%s is a %s\n", name, occupation)
 
 	default:
 		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
 	}
+
 }
 
 func main() {
